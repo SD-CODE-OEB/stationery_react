92accdfae2051cf133a31bf470f38dc04cbd8943
Syed Shoebuddin <ushoeb25@gmail.com>
2024-08-09 01:27:59 +0530

    update: cart layout and login updated

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,34 +1,80 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import products from "../Products/products.json";
 import { AppContext } from "../../context";
+import Container from "../../Container";
+import Row from "./Rows";
+import "./cart.css";
 
 const Cart = () => {
-  const { cartItem } = useContext(AppContext);
+  const PATH = process.env.REACT_APP_PATH;
+  const { cartItem, logged, user, setOrders, orders } = useContext(AppContext);
+  // store itemname, quantity, total price and useremail
+  const [item, setItem] = useState({});
+  const orderUpdate = () => {
+    item.email = user.email;
+    item.orderItems = cartItem;
+    setItem((prev) => ({ ...prev, item }));
+    setOrders((prev) => [...prev, item]);
+  };
+  const Navigate = useNavigate();
   return (
-    <>
-      <h1>Cart</h1>
+    <Container className="cart-container">
+      <h1 className="px-lg-4 heading">Cart</h1>
       <div className="container">
-        <div className="row">
-          {Object.keys(cartItem).map((id) => (
-            <div className="col-4">
-              <div className="card">
-                <img
-                  src={products[id].img}
-                  alt=""
-                  className="card-img-top"
-                  height={200}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{products[id].name}</h5>
-                  <p className="card-text">Price: ${products[id].price}</p>
-                  <p className="card-text">Quantity: {cartItem[id]}</p>
-                </div>
+        {Object.values(cartItem).every((val) => val === 0) ? (
+          <div className="container-fluid  d-flex flex-column justify-content-center align-items-center m-0 p-0 overflow-hidden">
+            <img
+              src="https://cdn.pixabay.com/photo/2015/10/22/16/42/icon-1001596_1280.png"
+              alt="empty cart"
+              className="img img-fluid"
+            />
+            <p className="fs-1 w-100 text-center ps-5 fw-bolder empty">
+              Cart is Empty!
+            </p>
+          </div>
+        ) : (
+          <div className="row">
+            <div className="col-lg-8">
+              <div className="row">
+                <div className="col-4 row-heads fs-5">Name</div>
+                <div className="col-4 row-heads fs-5">Quantity</div>
+                <div className="col-4 row-heads fs-5">Price</div>
               </div>
+              {Object.keys(cartItem).map((id) =>
+                !cartItem[id] ? (
+                  <></>
+                ) : (
+                  <Row products={products} id={id} key={id} />
+                )
+              )}
             </div>
-          ))}
-        </div>
+            <div className="col-lg-4">
+              <div className="h3">Submit Your Order</div>
+              {logged ? (
+                <button
+                  type="button"
+                  className="btn btn-dark"
+                  onClick={() => {
+                    Object.keys(cartItem).map((id) => orderUpdate());
+                  }}
+                >
+                  Submit Order {console.log(orders)}
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  className="btn btn-dark"
+                  onClick={() => Navigate(`${PATH}/login`)}
+                >
+                  Log In
+                </button>
+              )}
+            </div>
+          </div>
+        )}
       </div>
-    </>
+    </Container>
   );
 };
 
